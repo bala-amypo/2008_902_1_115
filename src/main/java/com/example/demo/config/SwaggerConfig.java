338cdf57a29be9ec554eaa5618fa7752d602dad6
bala-amypo <bala@amypo.in>
2025-12-18 09:38:31 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -19,25 +19,39 @@
 //         }
 // }
 
-
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
-                // Change the URL/port as per your server
-                .servers(List.of(
-                        new Server().url("https://9005.vs.amypo.ai")
-                ));
+                .info(new Info()
+                        .title("Cold Chain Temperature Breach Alert System")
+                        .version("1.0")
+                        .description("Swagger API documentation"))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("Authorization")
+                )
+                .components(
+                        new Components().addSecuritySchemes("Authorization", securityScheme)
+                );
     }
 }
