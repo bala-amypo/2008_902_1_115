500f29d074266fafa284ea9567187b135641b022
bala-amypo <bala@amypo.in>
2025-12-18 09:08:31 +0000

    User commit

--- src/main/java/com/example/demo/service/ShipmentRecordService.java
+++ src/main/java/com/example/demo/service/ShipmentRecordService.java
@@ -1,11 +1,17 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.ShipmentRecord;
 import java.util.List;
+import com.example.demo.entity.ShipmentRecord;
 
 public interface ShipmentRecordService {
+
     ShipmentRecord createShipment(ShipmentRecord shipment);
+
+    ShipmentRecord updateShipmentStatus(Long id, String status);
+
     ShipmentRecord getShipmentByCode(String code);
+
     ShipmentRecord getShipmentById(Long id);
+
     List<ShipmentRecord> getAllShipments();
 }
