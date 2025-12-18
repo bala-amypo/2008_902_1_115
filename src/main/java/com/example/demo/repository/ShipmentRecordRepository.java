c7db36a0fb7831570f555727562515de03743ca5
bala-amypo <bala@amypo.in>
2025-12-18 08:03:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/ShipmentRecordRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.ShipmentRecord;
+
+public interface ShipmentRecordRepository extends JpaRepository<ShipmentRecord, Long> {
+    ShipmentRecord findByShipmentCode(String code);
+}
---
+++ src/main/java/com/example/demo/service/ShipmentRecordServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.ShipmentRecord;
+import com.example.demo.repository.ShipmentRecordRepository;
+import com.example.demo.service.ShipmentRecordService;
+
+@Service
+public class ShipmentRecordServiceImpl implements ShipmentRecordService {
+
+    private final ShipmentRecordRepository repository;
+
+    public ShipmentRecordServiceImpl(ShipmentRecordRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ShipmentRecord createShipment(ShipmentRecord shipment) {
+        return repository.save(shipment);
+    }
+
+    @Override
+    public ShipmentRecord getShipmentByCode(String code) {
+        return repository.findByShipmentCode(code);
+    }
+
+    @Override
+    public ShipmentRecord getShipmentById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<ShipmentRecord> getAllShipments() {
+        return repository.findAll();
+    }
+}
